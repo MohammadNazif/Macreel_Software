bdbee6dfbc715198eaf759a81e0abfc4259bda07
Nidhi <navnidhi1210@gmail.com>
2026-01-18 14:03:45 +0530

    dfvd

--- macreel_software.client/src/app/feature/pages/employee/dashboard/dashboard.component.ts
+++ macreel_software.client/src/app/feature/pages/employee/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ManageDashboardService } from '../../../../core/services/manage-dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+//  dashboardCount!: EmpDashboardCount;
+  isLoading = true;
 
+  constructor(
+    private dashboardService: ManageDashboardService
+  ) {}
+
+  ngOnInit(): void {
+    this.getDashboardCount();
+  }
+
+  getDashboardCount(): void {
+    this.dashboardService.getEmpDashboardCount().subscribe({
+      next: (res) => {
+        // this.dashboardCount = res;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Dashboard count error', err);
+        this.isLoading = false;
+      }
+    });
+  }
 }
