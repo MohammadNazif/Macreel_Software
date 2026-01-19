fd5ae9b4e6999840da46d595035b734502e92a16
Nidhi <navnidhi1210@gmail.com>
2026-01-19 14:45:51 +0530

    dfgvdf

--- Macreel_Software.Server/Controllers/AdminController.cs
+++ Macreel_Software.Server/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Macreel_Software.DAL.Admin;
+﻿using Macreel_Software.Contracts.DTOs;
+using Macreel_Software.DAL.Admin;
 using Macreel_Software.Models;
 using Macreel_Software.Models.Common;
 using Macreel_Software.Models.Employee;
@@ -20,6 +21,8 @@ namespace Macreel_Software.Server.Controllers
         private readonly IWebHostEnvironment _env;
         private readonly MailSender _mailservice;
         private readonly PasswordEncrypt _pass;
+        private readonly int _userId;
+        private readonly int _roleId;
 
         public AdminController(
             IAdminServices service,
@@ -33,7 +36,13 @@ namespace Macreel_Software.Server.Controllers
             _env = env;
             _mailservice = mailservice;
             _pass = pass;
-        
+            var user = http.HttpContext?.User;
+            if (user != null && user.Identity?.IsAuthenticated == true)
+            {
+                _userId = Convert.ToInt32(user.FindFirst("UserId")?.Value);
+                int.TryParse(user.FindFirst("Role")?.Value, out _roleId);
+            }
+
         }
 
         [HttpGet("checkauth")]
@@ -486,7 +495,7 @@ namespace Macreel_Software.Server.Controllers
         {
             try
             {
-                ApiResponse<List<AssignLeaveDetails>> result =
+                ApiResponse<List<allAssignedLeave>> result =
                     await _services.getAllAssignedLeave(searchTerm, pageNumber, pageSize);
 
 
@@ -764,6 +773,8 @@ namespace Macreel_Software.Server.Controllers
         [HttpPost("insert-update-Task")]
         public async Task<IActionResult> insertUpdateTask([FromForm] Taskassign data)
         {
+            data.assignedBy = _userId;
+            int roleId = _roleId;
             if (data == null)
             {
                 return BadRequest(new
