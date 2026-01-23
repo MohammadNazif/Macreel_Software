a00270da4b56146edf6454c29310e633e01a7c2e
Nidhi <navnidhi1210@gmail.com>
2026-01-23 12:59:34 +0530

    fgnf

---
+++ Macreel_Software.Contracts/DTOs/AssignedProjectEmpListDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Macreel_Software.Contracts.DTOs
+{
+    public class AssignedProjectEmpListDto
+    {
+        public int? id { get; set; }
+        public int? projectId { get; set; }
+        public int? empId { get; set; }
+        public int? approveStatus { get; set; }
+        public string? empName { get; set; }
+        public string? designationName { get; set; }
+    }
+}
